13fb9548aee3c69ed05bce7d6b6a85ab4966bfd8
Brian Guthrie <btguthrie@gmail.com>
2012-09-05 14:37:18 -0400

    Removed namespacing.

--- lib/whiplash.rb
+++ lib/whiplash.rb
@@ -10,12 +10,8 @@ module Whiplash
   # Accepts:
   #   1. A 'hostname:port' String
   #   2. A 'hostname:port:db' String (to select the Redis db)
-  #   3. A 'hostname:port/namespace' String (to set the Redis namespace)
   #   4. A Redis URL String 'redis://host:port'
-  #   5. An instance of `Redis`, `Redis::Client`, `Redis::DistRedis`,
-  #      or `Redis::Namespace`.
-  #
-  # Borrowed from https://github.com/defunkt/resque/blob/master/lib/resque.rb.
+  #   5. An instance of `Redis`, or `Redis::Client`
   def self.redis=(server)
     case server
     when String
@@ -24,25 +20,17 @@ module Whiplash
       else
         server, namespace = server.split('/', 2)
         host, port, db = server.split(':')
-        redis = Redis.new(:host => host, :port => port,
-          :thread_safe => true, :db => db)
+        redis = Redis.new(host: host, port: port, thread_safe: true, db: db)
       end
-      namespace ||= :resque
 
-      @redis = Redis::Namespace.new(namespace, :redis => redis)
-    when Redis::Namespace
-      @redis = server
+      @redis = redis
     else
-      @redis = Redis::Namespace.new(:resque, :redis => server)
+      @redis = server
     end
-    @queues = Hash.new { |h,name|
-      h[name] = Resque::Queue.new(name, @redis, coder)
-    }
   end
 
   # Returns the current Redis connection. If none has been created, will
   # create a new one.
-  # Borrowed from https://github.com/defunkt/resque/blob/master/lib/resque.rb.
   def self.redis
     return @redis if @redis
     self.redis = Redis.respond_to?(:connect) ? Redis.connect : "localhost:6379"
